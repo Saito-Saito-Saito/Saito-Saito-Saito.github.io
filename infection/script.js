df89e34bf0b88af36548019f40e40767d13a44d3
Saito-Saito-Saito <last20c.boy@gmail.com>
2020-04-27 11:52:51 +0900

    sitemap created

--- infection/script.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2s');
-
-    ctx.strokeRect(0, 0, 80, 80);
-    ctx.fillRect(100, 0, 80, 80);
-    ctx.fillStyle = 'blue';
-});
